11a25114d0404bbbc61a14bf8abc8c05ebcca8d9
Alexander Redinger <redingera@gmail.com>
2019-11-07 15:26:38 -0500

    borrow someone else's

--- main.tf
+++ main.tf
@@ -1,2 +1,11 @@
-resource "null resource" "foo" {
+resource "random_id" "random" {
+  keepers = {
+    uuid = "${uuid()}"
+  }
+
+  byte_length = 8
+}
+
+output "random" {
+  value = "${random_id.random.hex}"
 }
