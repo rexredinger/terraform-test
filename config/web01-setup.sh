40334b25ddce84086f4f2aa0c213fd97081e22f2
Alexander Redinger <redingera@gmail.com>
2019-11-08 18:26:08 -0500

    Create web01-setup.sh

---
+++ config/web01-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Deploys NGINX
+
+# cd /tmp
+apt-get -y update > /dev/null 2>&1
+apt install -y nginx > /dev/null 2>&1
+
+cat << EOF > /var/www/html/index.nginx-debian.html
+<html>
+  <head><title>Wolfer Terraform Demo!</title></head>
+  Hello from Web01!
+  </body>
+</html>
+EOF
+
+echo "Provisioning Complete!"
